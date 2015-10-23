6f02412e63f034e13a99a0d6c4519c698e0c2a14
root <root@jack-VirtualBox.(none)>
2015-10-23 11:42:55 +0100

    Creado el programa byebyeWorld.rb en Ruby para usar con pry

---
+++ byebyeWorld.rb
@@ -0,0 +1,16 @@
+#byebyeWorld.rb
+require 'pry'
+
+#define a method
+def byebye()
+	puts "Bye bye World!!!"
+end
+
+#set x to 10
+x=10
+
+#start a REPL session
+binding.pry
+
+#program resumes here (after pry session)
+puts "Program resumes here. Value of x is: #{x}."
